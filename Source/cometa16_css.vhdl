e13efefe99704b32fdce3569a57cfc58e5efc366
Ícaro Gabryel <icarogabryel2001@gmail.com>
2023-06-08 00:15:09 -0300

    css up

--- Source/cometa16_css.vhdl
+++ Source/cometa16_css.vhdl
@@ -37,19 +37,35 @@ entity cometa16_css is
 end cometa16_css;
 
 architecture behaviior_css of cometa16_css is
+    type cores_in_vector is array(0 to 3) of std_logic_vector(129 downto 0);
+    type cores_out_vector is array(0 to 3) of std_logic_vector(144 downto 0);
+    
     signal priority: integer =: 0;
     
     signal requests: std_logic_vector(0 to 3);
-    signal cores_in: std_logic_vector(0 to 3)(129 downto 0);
-    signal cores_out: std_logic_vector(0 to 3)(144 downto 0);
+    signal have_request: std_logic;
+    
+    signal cores_in: cores_in_vector;
+    signal cores_out: cores_out_vector;
 
 begin
     resquests <= core0_out(144) or core0_out(143) & core1_out(144) or core1_out(143) & core2_out(144) or core2_out(143) & core3_out(144) or core3_out(143);
-    
+    have_request <= requests(0) or requests(1) or requests(2) or requests(3);
+
+    cores_in(0) <= core0_in;
+    cores_in(1) <= core1_in;
+    cores_in(2) <= core2_in;
+    cores_in(3) <= core3_in;
+
+    cores_out(0) <= core0_out;
+    cores_out(1) <= core1_out;
+    cores_out(2) <= core2_out;
+    cores_out(3) <= core3_out;
+
     round_process: process(priority, request, request0, request1, request2, request3)
     
     begin
-        if (request = '1') then
+        if (request = '1') then -- mudar sinal de request
             for i in 0 to 3 loop
                 if (requests((priority + i) % 4) = '1') then
                     cores_in((priority + i) % 4) <= main_mem_out;
