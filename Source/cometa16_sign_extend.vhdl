260a2ceda0540036645a3a01e556942f8cf9c565
Ícaro Gabryel <91147230+icarogby@users.noreply.github.com>
2024-02-28 15:33:39 -0300

    all AL ops ok

--- Source/cometa16_sign_extend.vhdl
+++ Source/cometa16_sign_extend.vhdl
@@ -29,9 +29,9 @@ architecture behavior_sign_extend of cometa16_sign_extend is
 
 begin
     with ctrl_sign_extend select sign_extend_out <=
-        "00000000" & imm(7 downto 0)              when "00",
-        imm(7 downto 0) & "00000000"              when "01",
-        (15 downto 8 => imm(7)) & imm(7 downto 0) when "11",
-        "XXXXXXXXXXXXXXXX"                        when others;
+        "00000000" & imm(7 downto 0)         when "00",
+        imm(7 downto 0) & "00000000"         when "01",
+        (0 to 7 => imm(7)) & imm(7 downto 0) when "11",
+        "XXXXXXXXXXXXXXXX"                   when others;
 
 end behavior_sign_extend;
